60bdb72da30621b4a49594a085f7c04797ba18fa
Vojtech Bocek <vbocek@gmail.com>
2015-03-04 01:07:02 +0100

    Add mach-shamu

--- kexec/arch/arm/mach.c
+++ kexec/arch/arm/mach.c
@@ -2,8 +2,10 @@
 #include "mach.h"
 
 extern const struct arch_mach arm_mach_hammerhead;
+extern const struct arch_mach arm_mach_shamu;
 static const struct arm_mach *const arm_machs[] = {
     &arm_mach_hammerhead,
+    &arm_mach_shamu,
     NULL
 };
 // update zImage_arm_usage when modifying this.
