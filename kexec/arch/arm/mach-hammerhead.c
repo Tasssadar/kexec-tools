d05549abc319f2d64c54d1446b69fe3c332ad2dd
Parth Bhatia <parthbhatia98@gmail.com>
2015-09-04 21:04:24 +0530

    Add support for Xiaomi Redmi 1S (armani)

--- kexec/arch/arm/mach-hammerhead.c
+++ kexec/arch/arm/mach-hammerhead.c
@@ -188,7 +188,7 @@ static int hammerhead_add_extra_regs(void *dtb_buf)
 }
 
 const struct arm_mach arm_mach_hammerhead = {
-    .boardnames = { "hammerhead", "bacon", "d851", "d855", NULL },
+    .boardnames = { "hammerhead", "bacon", "d851", "d855", "armani", NULL },
     .choose_dtb = hammerhead_choose_dtb,
     .add_extra_regs = hammerhead_add_extra_regs,
 };
